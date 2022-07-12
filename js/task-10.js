cecd007b1276d9b5cfbd5aa1c79e985374b9907c
Oksana Krotova <oksi.fox.post@gmail.com>
2022-07-12 19:21:34 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,35 @@
+
+
+/// я не змогла зробити((
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+
+const input = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const divBoxes = document.querySelector('#boxes');
+
+const setBoxes = () => {
+  amount = Number(input.value);
+  createBoxes()
+};
+
+const removeBoxes = () => {
+divBoxes.innerHTML = '';
+};
+
+createBtn.addEventListener('click',setBoxes);
+destroyBtn.addEventListener('click', removeBoxes);
+
+function createBoxes(amount) {
+  const firstSize = 30;
+  for (let i= 0; i<=amount; i +=1) {
+   divBoxes.insertAdjacentHTML('beforeend',
+    `<div style='width: ${firstSize+i*10};
+   height: ${firstSize + i*10};
+   background-color: ${getRandomHexColor()};'></div`);
+  }
+}
 
